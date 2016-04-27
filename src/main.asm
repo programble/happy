dfe8668b62693aae5dbccba9fd435cd6857ee5d7
Curtis McEnroe <curtis.mcenroe@adgear.com>
2016-04-27 15:45:13 -0400

    vga.curs

--- src/main.asm
+++ src/main.asm
@@ -1,5 +1,5 @@
 global main
-extern vga.~buf, vga.blank, vga.puts
+extern vga.~buf, vga.blank, vga.puts, vga.curs
 %include "vga.mac"
 
 section .rodata
@@ -9,10 +9,13 @@ main.#hello equ $ - main.~hello
 section .text
 main:
   xor ax, ax
+  mov ah, vga.GRY
   call vga.blank
   mov ah, vga.RED
   mov ecx, main.#hello
   mov esi, main.~hello
   mov edi, vga.~buf
   call vga.puts
+  mov ah, 0x0D
+  call vga.curs
   ret
--- src/vga.asm
+++ src/vga.asm
@@ -1,5 +1,5 @@
 global vga.~buf, vga.$buf, vga.#buf
-global vga.blank, vga.puts
+global vga.blank, vga.puts, vga.curs
 %include "vga.mac"
 
 vga.~buf equ 0xB8000
@@ -16,4 +16,29 @@ vga.puts: ; ah(attr) ecx(len) esi(str) edi(buf) : edi(buf) : al ecx esi
   lodsb
   stosw
   loop vga.puts
+  ret
+
+vga.curs: ; ah(shape) edi(buf) : : al dx
+  mov dx, 0x3D4
+  mov al, 0x0A
+  out dx, al
+  inc dx
+  xchg al, ah
+  out dx, al
+  lea ecx, [edi - vga.~buf]
+  shr ecx, 1
+  dec dx
+  mov al, 0x0F
+  out dx, al
+  inc dx
+  xchg ax, cx
+  out dx, al
+  xchg cx, ax
+  dec dx
+  dec al
+  out dx, al
+  inc dx
+  xchg ax, cx
+  shr ax, 8
+  out dx, al
   ret
