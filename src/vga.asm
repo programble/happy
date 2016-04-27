db20faa3e8e4d2168f071b87e064f3590fe7022a
Curtis McEnroe <programble@gmail.com>
2016-04-27 19:26:39 -0400

    Doc vga macros

--- src/vga.asm
+++ src/vga.asm
@@ -1,5 +1,5 @@
 global vga.~buf, vga.$buf, vga.#buf
-global vga._blank, vga._putq, vga._puts, vga.aputs, vga.curs
+global vga._blank, vga._putq, vga._puts
 %include "vga.mac"
 
 vga.~buf equ 0xB8000
@@ -30,35 +30,4 @@ vga._puts: ; ecx(len) esi(str) edi(buf) : edi(buf) : ecx esi
   movsb
   inc edi
   loop vga._puts
-  ret
-
-vga.aputs: ; ah(attr) ecx(len) esi(str) edi(buf) : edi(buf) : al ecx esi
-  lodsb
-  stosw
-  loop vga.aputs
-  ret
-
-vga.curs: ; ah(shape) edi(buf) : : al dx
-  mov dx, 0x3D4
-  mov al, 0x0A
-  out dx, al
-  inc dx
-  xchg al, ah
-  out dx, al
-  lea ecx, [edi - vga.~buf]
-  shr ecx, 1
-  dec dx
-  mov al, 0x0F
-  out dx, al
-  inc dx
-  xchg ax, cx
-  out dx, al
-  xchg cx, ax
-  dec dx
-  dec al
-  out dx, al
-  inc dx
-  xchg ax, cx
-  shr ax, 8
-  out dx, al
   ret
