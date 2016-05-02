37324d69c1f5aaa5b8079c5b44950424cbdc68bc
Curtis McEnroe <programble@gmail.com>
2016-05-02 00:10:13 -0400

    Output flag mnemonics in panic

--- src/panic.asm
+++ src/panic.asm
@@ -2,6 +2,24 @@ global _panic
 extern boot.$stack, elf.sym, fmt.bin, fmt.dec, fmt.hex, vga.attr, vga.print
 %include "vga.mac"
 
+Eflags.CF equ 1
+Eflags.PF equ 4
+Eflags.AF equ 0x10
+Eflags.ZF equ 0x40
+Eflags.SF equ 0x80
+Eflags.TF equ 0x100
+Eflags.IF equ 0x200
+Eflags.DF equ 0x400
+Eflags.OF equ 0x800
+; TODO: IOPL
+Eflags.NT equ 0x4000
+Eflags.RF equ 0x10000
+Eflags.VM equ 0x20000
+Eflags.AC equ 0x40000
+Eflags.VIF equ 0x80000
+Eflags.VIP equ 0x100000
+Eflags.ID equ 0x200000
+
 struc Pushad, -0x1C
   .eax: resd 1
   .ecx: resd 1
@@ -19,6 +37,14 @@ endstruc
   %%str: db %1, 0
   __SECT__
   mov esi, %%str
+%endmacro
+
+%macro _flag 2
+  test dword [esp], Eflags.%1
+  jz %%else
+  string %2
+  call vga.print
+  %%else:
 %endmacro
 
 %macro _reg 2
@@ -52,8 +78,24 @@ _panic: ; eax(eip) ecx(line) edx(file) esi(msg) : :
   string `\neflags `
   call vga.print
   mov eax, [esp]
-  call fmt.bin
+  call fmt.hex
   call vga.print
+  _flag CF, ' CF'
+  _flag PF, ' PF'
+  _flag AF, ' AF'
+  _flag ZF, ' ZF'
+  _flag SF, ' SF'
+  _flag TF, ' TF'
+  _flag IF, ' IF'
+  _flag DF, ' DF'
+  _flag OF, ' OF'
+  _flag NT, ' NT'
+  _flag RF, ' RF'
+  _flag VM, ' VM'
+  _flag AC, ' AC'
+  _flag VIF, ' VIF'
+  _flag VIP, ' VIP'
+  _flag ID, ' ID'
   add esp, 4
 
   _reg `\neax `, eax
