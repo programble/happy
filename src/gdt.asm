46829c705e68bfde52f190929a795af4f12d84b7
Curtis McEnroe <programble@gmail.com>
2016-05-04 19:54:14 -0400

    Rewrite gdt

--- src/gdt.asm
+++ src/gdt.asm
@@ -1,32 +1,35 @@
 global gdt.init
 
-Type.A equ 0b0000_0001
-Type.W equ 0b0000_0010
-Type.E equ 0b0000_0100
-Type.R equ 0b0000_0010
-Type.C equ 0b0000_0100
-Type.DATA equ 0b0000_0000
-Type.CODE equ 0b0000_1000
+Type:
+  .DATA: equ 0000_0000b
+  .CODE: equ 0000_1000b
+  .A: equ 0000_0001b
+  .W: equ 0000_0010b
+  .E: equ 0000_0100b
+  .R: equ 0000_0010b
+  .C: equ 0000_0100b
 
-Access.S equ 0b0001_0000
-Access.DPL0 equ 0b0000_0000
-Access.DPL1 equ 0b0010_0000
-Access.DPL2 equ 0b0100_0000
-Access.DPL3 equ 0b0110_0000
-Access.P equ 0b1000_0000
+Access:
+  .S: equ 0001_0000b
+  .DPL0: equ 0000_0000b
+  .DPL1: equ 0010_0000b
+  .DPL2: equ 0100_0000b
+  .DPL3: equ 0110_0000b
+  .P: equ 1000_0000b
 
-Flags.L equ 0b0010_0000
-Flags.D equ 0b0100_0000
-Flags.B equ 0b0100_0000
-Flags.G equ 0b1000_0000
+Flags:
+  .L: equ 0010_0000b
+  .D: equ 0100_0000b
+  .B: equ 0100_0000b
+  .G: equ 1000_0000b
 
-struc Entry
-  .limit_lo: resw 1
-  .base_lo: resw 1
-  .base_mi: resb 1
-  .type_access: resb 1
-  .limit_hi_flags: resb 1
-  .base_hi: resb 1
+struc SegmentDescriptor
+  .limitLow: resw 1
+  .baseLow: resw 1
+  .baseMid: resb 1
+  .typeAccess: resb 1
+  .limitHighFlags: resb 1
+  .baseHigh: resb 1
 endstruc
 
 struc Gdt
@@ -35,41 +38,43 @@ struc Gdt
 endstruc
 
 section .rodata
-gdt.~entries:
-  .null: dq 0
+gdt.table:
+  .null:
+    dq 0
   .code:
-    istruc Entry
-      at Entry.limit_lo, dw 0xFFFF
-      at Entry.base_lo, dw 0
-      at Entry.base_mi, db 0
-      at Entry.type_access, db Type.CODE | Type.R | Access.S | Access.DPL0 | Access.P
-      at Entry.limit_hi_flags, db 0xF | Flags.D | Flags.G
-      at Entry.base_hi, db 0
+    istruc SegmentDescriptor
+      at SegmentDescriptor.limitLow, dw 0FFFFh
+      at SegmentDescriptor.baseLow, dw 0
+      at SegmentDescriptor.baseMid, db 0
+      at SegmentDescriptor.typeAccess, db Type.CODE | Type.R | Access.S | Access.DPL0 | Access.P
+      at SegmentDescriptor.limitHighFlags, db 0Fh | Flags.D | Flags.G
+      at SegmentDescriptor.baseHigh, db 0
     iend
   .data:
-    istruc Entry
-      at Entry.limit_lo, dw 0xFFFF
-      at Entry.base_lo, dw 0
-      at Entry.base_mi, db 0
-      at Entry.type_access, db Type.DATA | Type.W | Access.S | Access.DPL0 | Access.P
-      at Entry.limit_hi_flags, db 0xF | Flags.B | Flags.G
-      at Entry.base_hi, db 0
+    istruc SegmentDescriptor
+      at SegmentDescriptor.limitLow, dw 0FFFFh
+      at SegmentDescriptor.baseLow, dw 0
+      at SegmentDescriptor.baseMid, db 0
+      at SegmentDescriptor.typeAccess, db Type.DATA | Type.W | Access.S | Access.DPL0 | Access.P
+      at SegmentDescriptor.limitHighFlags, db 0Fh | Flags.B | Flags.G
+      at SegmentDescriptor.baseHigh, db 0
     iend
-gdt.#entries equ $ - gdt.~entries
+  .#: equ $ - gdt.table
 gdt.gdt:
   istruc Gdt
-    at Gdt.limit, dw gdt.#entries - 1
-    at Gdt.base, dd gdt.~entries
+    at Gdt.limit, dw gdt.table.# - 1
+    at Gdt.base, dd gdt.table
   iend
 
 section .text
 gdt.init: ; : : ax
   lgdt [gdt.gdt]
-  mov eax, gdt.~entries.data - gdt.~entries
+  mov ax, gdt.table.data - gdt.table
   mov ds, ax
+  mov ss, ax
   mov es, ax
   mov fs, ax
   mov gs, ax
-  mov ss, ax
-  jmp (gdt.~entries.code - gdt.~entries):.ret
-  .ret: ret
+  jmp (gdt.table.code - gdt.table):.ret
+  .ret:
+  ret
