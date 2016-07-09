0245d41c1f802680a341dee73f2afb6c225ea520
Curtis McEnroe <programble@gmail.com>
2016-07-09 18:04:27 -0400

    Replace main prompt with elf.symbolString call

--- src/main.asm
+++ src/main.asm
@@ -1,9 +1,7 @@
-global main.main
-extern vga.attribute, kbd.readLine, str.equal?
-extern vga.blank, core.halt, kbd.reset
-extern diag.printEflags, diag.printRegs, diag.printStack
-extern mboot.printInfo, mboot.printMmap
-extern fmt.printBuffers, kbd.printBuffers
+global main.main, main.clear, main.panic, main.eflags, main.regs, main.stack
+extern vga.attribute, kbd.readLine
+extern elf.stringSymbol
+extern vga.blank, diag.printEflags, diag.printRegs, diag.printStack
 %include "macro.mac"
 %include "core.mac"
 %include "vga.mac"
@@ -14,34 +12,6 @@ section .text
 main.main: ; : : *
   _write `You'll never be %hd0 %hd1.\n`, 0DEADBEEFh, 0CAFEBABEh
 
-  %macro _cmdStart 0
-    [section .rodata]
-    %%cmdString:
-    %define _CMD_STRING %%cmdString
-    __SECT__
-  %endmacro
-
-  %macro _cmd 2
-    _string edx, edi, %1
-    [section .rodata]
-    db ' '
-    __SECT__
-    _push ecx, esi
-    call str.equal?
-    _pop ecx, esi
-    jne %%cmdElse
-    push .next
-    jmp %2
-    %%cmdElse:
-  %endmacro
-
-  %macro _cmdEnd 0
-    [section .rodata]
-    %%cmdString.#: equ $ - _CMD_STRING
-    %define _CMD_STRING_LEN %%cmdString.#
-    __SECT__
-  %endmacro
-
   .prompt:
     _writeChar '>'
     mov byte [vga.attribute], vga.Color.GRAY | vga.Color.BRIGHT
@@ -49,36 +19,16 @@ main.main: ; : : *
     call kbd.readLine
     mov byte [vga.attribute], vga.Color.GRAY
 
-    _cmdStart
-      _cmd 'help', main.help
-      _cmd 'clear', main.clear
+    call elf.stringSymbol
+    test eax, eax
+    jnz .call
+    _write `?\n`
+    jmp .prompt
 
-      _cmd 'halt', core.halt
-      _cmd 'panic', main.panic
-      _cmd 'reset', kbd.reset
-
-      _cmd 'eflags', main.eflags
-      _cmd 'regs', main.regs
-      _cmd 'stack', main.stack
-
-      _cmd 'mboot', mboot.printInfo
-      _cmd 'mmap', mboot.printMmap
-
-      _cmd 'fmt', fmt.printBuffers
-      _cmd 'kbd', kbd.printBuffers
-    _cmdEnd
-
-    _writeChar '?'
-
-    .next:
+    .call:
+    call eax
     _writeChar `\n`
   jmp .prompt
-
-main.help: ; : : *
-  mov ecx, _CMD_STRING_LEN
-  mov esi, _CMD_STRING
-  _write
-ret
 
 main.clear: ; : : *
   call vga.blank
